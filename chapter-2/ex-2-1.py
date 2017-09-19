4387dafc20d40954269087a7e6355942ab085435
Jenna Runge <jenna-runge@student.kirkwood.edu>
2017-09-19 13:17:42 +0000

    Address Program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,20 +6,18 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Dean Winchester")
 
 # display the address
-
+print("6402 Mistletow Lane")
 
 # display the city, state and zip
-
+print("Larence, Kansas, 66044")
 
 # display the phone number
-
+print("678-865-7469")
 
 # display the course area
-
-
-
+print("CIS-121")
 
 
