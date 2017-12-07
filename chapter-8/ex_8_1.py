5c7f617c17163a059c83a1046798132e531a8c77
Jenna Runge <jenna-runge@student.kirkwood.edu>
2017-12-07 01:22:27 +0000

    Updating

--- chapter-8/ex_8_1.py
+++ chapter-8/ex_8_1.py
@@ -9,19 +9,28 @@
 def main():
     
     # Get a full name as user input
-    name = input("Enter your full name: ")
+    name = input("Enter your first and last name: ")
 
     # Split the string on spaces into a list of name parts and assign to a variable
-    
-
+    name_s = name.split(" ")
     # iterate over each name part in the list
-
+    name_s_zero = name_s[0]
+    name_s_zero = name_s_zero.replace(name_s_zero[0], name_s_zero[0].upper())
+    name_s_one = name_s[1]
+    name_s_one = name_s_one.replace(name_s_one[0], name_s_one[0].upper())
+    print(name_s_zero, name_s_one)    
         # display the first character ( parts[0] ) of the name part as upper case
 
-        # follow the character with a period (all these should be on the same line)
-
-
-        
 # Call the main function.
 main()
 
+
+
+#SCRATCH/ SCRATCH/ SCRATCH
+s = "Hello World"
+index= s.index("World")
+
+number = [1,2,3,4]
+numbers[0] = 9
+#S[6] = "p" NOT LEGAL
+c= s[index:11]
